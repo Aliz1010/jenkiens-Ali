66dfb832ef802b425172b0345e30da84e5f071b4
Aliz1010 <alialmtyre327@gmail.com>
2025-09-03 00:14:54 -0900

    abo 3abd 3mk

--- src/main/java/org/example/CalculatorApp.java
+++ src/main/java/org/example/CalculatorApp.java
@@ -3,7 +3,7 @@ package org.example;
 public class CalculatorApp {
     public static void main(String[] args) {
         int x = Calculator.add(2,3);
-
+        // Abo 3abed
         int y = Calculator.subtract(2,3);
 
         System.out.println(x + ", " + y);
